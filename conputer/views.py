1e1c3c8afe89f4ba52783003bcf3f53db6fe62db
fear <roma.bele.aleksandrovich@gmail.com>
2026-04-07 01:19:26 +0300

    на странице создан поиск, доделаны картинки

--- conputer/views.py
+++ conputer/views.py
@@ -5,10 +5,32 @@ from .models import Component, Component_category, Brand
 
 
 def component_list(request):
+    allowed_sort = ['price', '-price', 'name', 'id']
+
+    query = request.GET.get('q')
+
+    sort_by = request.GET.get('sort', 'id')
+    
+    if sort_by not in allowed_sort:
+        sort_by = 'id'
+
+    categories = Component_category.objects.all()
+
     components = Component.objects.all().select_related('brand', 'category')
-    categories = Component_category.objects.all() # Получаем все категории
+
+    if query:
+        components = components.filter(
+            Q(name__icontains=query) | 
+            Q(description__icontains=query) |
+            Q(brand__name__icontains=query)
+        )
+
+    components = components.order_by(sort_by)
+
     return render(request, 'conputer/list.html', {
         'components': components,
-        'categories': categories 
+        'categories': categories,
+        'current_sort': sort_by,
+        'query': query
     })
 
